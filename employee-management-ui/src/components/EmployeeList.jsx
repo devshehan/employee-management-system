5a1e5b48cd1f840cee9fc8553c46c75690fcae7f
devshehan <shehaan.avishka00@gmail.com>
2023-09-06 08:00:42 +0530

    create a table

--- employee-management-ui/src/components/EmployeeList.jsx
+++ employee-management-ui/src/components/EmployeeList.jsx
@@ -5,7 +5,7 @@ const EmployeeList = () => {
   const navigate = useNavigate();
   return (
     <div className="">
-      <div className="mx-auto p-8 bg-cyan-200">
+      <div className="mx-auto p-8 ">
         <button
           onClick={() => navigate("/addEmployee")}
           className="bg-green-400 p-2 px-6 rounded hover:bg-green-700 text-white"
@@ -13,7 +13,55 @@ const EmployeeList = () => {
           Add New Employee
         </button>
 
-        
+        <div className="flex shadow border-b mt-4">
+          <table className="min-w-full">
+            <thead>
+              <tr>
+                <th className="text-left text-gray-500  px-6 py-3 ">
+                  First Name
+                </th>
+                <th className="text-left text-gray-500  px-6 py-3 ">
+                  Last Name
+                </th>
+                <th className="text-left text-gray-500  px-6 py-3 ">
+                  Email ID
+                </th>
+                <th className=" text-gray-500  px-6 py-3 ">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="text-left font-thin px-6 py-4">
+                  <div>Shehaan</div>
+                </td>
+                <td className="text-left font-thin px-6 py-4">
+                  <div>Avishka</div>
+                </td>
+                <td className="text-left font-thin px-6 py-4">
+                  <div>devshehan@gmail.com</div>
+                </td>
+                <td className="flex justify-center px-4">
+                  <div className="space-x-4">
+                    <button
+                      // onClick={handleSubmit}
+                      className="bg-green-400 p-2 px-6 rounded hover:bg-green-700 text-white"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      // onClick={clearForm}
+                      className="bg-red-400 p-2 px-6 rounded hover:bg-red-700 text-white"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
